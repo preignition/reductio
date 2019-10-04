10ba2e6724dae3ec4d25e9c32e02b474ff8e5029
christophe-g <christophe.geiser@gmail.com>
2019-10-04 18:05:57 +0200

    make one first test pass (alias.spec.js)

--- karma.conf.js
+++ karma.conf.js
@@ -8,28 +8,39 @@ module.exports = function(config) {
 		'node_modules/crossfilter2/crossfilter.min.js',
 		'node_modules/lodash/index.js',
 		'reductio.min.js',
-		'test/**/*.spec.js'
+     { pattern: config.grep ? config.grep : 'test/**/*.test.js', type: 'module' },
+		// 'test/**/*.test.js'
     ],
 
 	browsers: [
 		// 'Chrome'
 		// 'ChromeCanary',
 		// 'Firefox',
-		'PhantomJS'
+    'ChromeHeadless'
+		// 'PhantomJS'
 		// 'PhantomJS'
 	],
 
 	plugins: [
 		'karma-jasmine',
 		'karma-chrome-launcher',
-		'karma-firefox-launcher',
-		'karma-phantomjs-launcher'
+		// 'karma-firefox-launcher',
+		// 'karma-phantomjs-launcher'
 	],
 
-	reporters: ['dots'],
+	// reporters: ['dots'],
+  reporters: ['progress'],
 
 	singleRun: true,
 
+  concurrency: Infinity,
+  autoWatch: false,
+  colors: true,
+  port: 9876,  // karma web server port
 	captureTimeout: 60000
   });
 };
+
+    // logLevel: config.LOG_INFO,
+    // browsers: ['ChromeHeadless'],
+    // // singleRun: false, // Karma captures browsers, runs the tests and exits
